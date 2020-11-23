62b06e2a0300d9f13f927d67f86ad48e9f4a0459
EwigLicht <netl992017@163.com>
2020-11-23 17:34:25 +0800

    Site updated: 2020-11-23 17:34:24

--- Licht/js/lichtool.js
+++ Licht/js/lichtool.js
@@ -1,3 +1,52 @@
+//生成随机rgba颜色
 function randomColor() {
     return "rgba(" + parseInt(Math.random() * 256) + "," + parseInt(Math.random() * 256) + "," + parseInt(Math.random() * 256) + ",1" + ")";
 }
+
+//跨浏览器兼容阻止默认行为
+function preDef(ev) {
+    return ev.preventDefault ? ev.preventDefault() : window.event.returnValue = false;
+}
+
+//跨浏览器兼容阻止事件冒泡函数
+function stopBubble(e) {
+    e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true;
+}
+
+// 拖拽窗口函数（限制出界）
+function dragAble(node) {
+    node.onmousedown = function (ev) {
+        var e = ev || window.event;
+        var offsetX = e.clientX - node.offsetLeft;
+        var offsetY = e.clientY - node.offsetTop;
+
+        document.onmousemove = function (ev) {
+            var e = ev || window.event;
+            var l = e.clientX - offsetX;
+            var t = e.clientY - offsetY;
+            var windowWidth = document.documentElement.clientWidth || document.body.clientWidth;
+            var windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+
+            if (l <= 0) {
+                l = 0;
+            }
+            if (l >= windowWidth - node.offsetWidth) {
+                l = windowWidth - node.offsetWidth;
+            }
+            if (t <= 0) {
+                t = 0;
+            }
+            if (t >= windowHeight - node.offsetHeight) {
+                t = windowHeight - node.offsetHeight;
+            }
+
+            node.style.left = l + "px";
+            node.style.top = t + "px";
+        }
+    }
+    document.onmouseup = function (ev) {
+        document.onmousemove = null;
+    }
+}
+
+
