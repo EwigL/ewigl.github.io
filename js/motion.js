80d8169af86b9f0605f4f6690a98af5ced9c4d4f
EwigLicht <ewiglichtl@163.com>
2021-01-13 21:56:47 +0800

    Site updated: 2021-01-13 21:56:47

--- js/motion.js
+++ js/motion.js
@@ -4,22 +4,22 @@ NexT.motion = {};
 
 NexT.motion.integrator = {
   queue: [],
-  init: function () {
+  init : function() {
     this.queue = [];
     return this;
   },
-  add: function (fn) {
+  add: function(fn) {
     const sequence = fn();
     if (CONFIG.motion.async) this.queue.push(sequence);
     else this.queue = this.queue.concat(sequence);
     return this;
   },
-  bootstrap: function () {
+  bootstrap: function() {
     if (!CONFIG.motion.async) this.queue = [this.queue];
     this.queue.forEach(sequence => {
       const timeline = window.anime.timeline({
         duration: 100,
-        easing: 'linear'
+        easing  : 'linear'
       });
       sequence.forEach(item => {
         if (item.deltaT) timeline.add(item, item.deltaT);
@@ -30,15 +30,15 @@ NexT.motion.integrator = {
 };
 
 NexT.motion.middleWares = {
-  header: function () {
+  header: function() {
     const sequence = [];
 
     function getMistLineSettings(targets) {
       sequence.push({
         targets,
-        scaleX: [0, 1],
+        scaleX  : [0, 1],
         duration: 500,
-        deltaT: '-=200'
+        deltaT  : '-=200'
       });
     }
 
@@ -46,8 +46,8 @@ NexT.motion.middleWares = {
       sequence.push({
         targets,
         opacity: 1,
-        top: 0,
-        deltaT: sequenceQueue ? '-=200' : '-=0'
+        top    : 0,
+        deltaT : sequenceQueue ? '-=200' : '-=0'
       });
     }
 
@@ -63,14 +63,14 @@ NexT.motion.middleWares = {
       sequence.push({
         targets,
         complete: () => targets.classList.add('animated', 'fadeInDown'),
-        deltaT: '-=200'
+        deltaT  : '-=200'
       });
     });
 
     return sequence;
   },
 
-  subMenu: function () {
+  subMenu: function() {
     const subMenuItem = document.querySelectorAll('.sub-menu .menu-item');
     if (subMenuItem.length > 0) {
       subMenuItem.forEach(element => {
@@ -80,7 +80,7 @@ NexT.motion.middleWares = {
     return [];
   },
 
-  postList: function () {
+  postList: function() {
     const sequence = [];
     const { post_block, post_header, post_body, coll_header } = CONFIG.motion.transition;
 
@@ -90,7 +90,7 @@ NexT.motion.middleWares = {
         sequence.push({
           targets,
           complete: () => targets.classList.add('animated', animation),
-          deltaT: '-=100'
+          deltaT  : '-=100'
         });
       });
     }
@@ -103,20 +103,20 @@ NexT.motion.middleWares = {
     return sequence;
   },
 
-  sidebar: function () {
+  sidebar: function() {
     const sidebar = document.querySelector('.sidebar');
     const sidebarTransition = CONFIG.motion.transition.sidebar;
     // Only for Pisces | Gemini.
     if (sidebarTransition && (CONFIG.scheme === 'Pisces' || CONFIG.scheme === 'Gemini')) {
       return [{
-        targets: sidebar,
+        targets : sidebar,
         complete: () => sidebar.classList.add('animated', sidebarTransition)
       }];
     }
     return [];
   },
 
-  footer: function () {
+  footer: function() {
     return [{
       targets: document.querySelector('.footer'),
       opacity: 1
